d7309679e9b73212517982834dfa8f0affa4ffd1
IT21826740 <it21826740@my.sliit.lk>
2025-01-02 17:36:25 +0530

    Create student entity

---
+++ src/main/java/lk/zerocode/student_payment_management_system/models/Student.java
@@ -0,0 +1,69 @@
+package lk.zerocode.student_payment_management_system.models;
+
+import jakarta.persistence.*;
+import lk.zerocode.student_payment_management_system.models.options.Gender;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long id;
+
+    @Column(name = "fiscal_code", nullable = false)
+    private String fiscalCode;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate birthday;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(name = "special_needs", nullable = false)
+    private String specialNeeds;
+
+    @ElementCollection
+    @CollectionTable(name = "student_mobile_numbers", joinColumns = @JoinColumn(name = "student_id"))
+    @Column(name = "mobile_number")
+    private List<String> mobileNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "student_address_id", nullable = false)
+    private StudentAddress studentAddress;
+
+    @ManyToOne
+    @JoinColumn(name = "guardian_id", referencedColumnName = "guardian_id", nullable = false)
+    private Guardian guardian;
+
+    @Column(nullable = false)
+    private String email;
+
+    public Student() {}
+
+    public Student(Long id, String fiscalCode, String name, LocalDate birthday, Gender gender, String country, String specialNeeds, List<String> mobileNumber, StudentAddress studentAddress, String email) {
+        this.id = id;
+        this.fiscalCode = fiscalCode;
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.country = country;
+        this.specialNeeds = specialNeeds;
+        this.mobileNumber = mobileNumber;
+        this.studentAddress = studentAddress;
+        this.email = email;
+    }
+}
---
+++ src/main/java/lk/zerocode/student_payment_management_system/models/options/Gender.java
@@ -0,0 +1,5 @@
+package lk.zerocode.student_payment_management_system.models.options;
+
+public enum Gender {
+    MALE,FEMALE
+}
