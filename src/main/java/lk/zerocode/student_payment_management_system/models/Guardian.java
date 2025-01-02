feef276c57fe8212782cd069d9462803e23492bd
IT21826740 <it21826740@my.sliit.lk>
2025-01-02 17:35:10 +0530

    Create guardian entity

---
+++ src/main/java/lk/zerocode/student_payment_management_system/models/Guardian.java
@@ -0,0 +1,49 @@
+package lk.zerocode.student_payment_management_system.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "guardians")
+public class Guardian {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "guardian_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "fiscal_code", nullable = false)
+    private String fiscalCode;
+
+    @Column(nullable = false)
+    private String relation;
+
+    @ElementCollection
+    @CollectionTable(name = "guardian_mobile_numbers", joinColumns = @JoinColumn(name = "guardian_id"))
+    @Column(name = "mobile_number")
+    private List<String> mobileNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", nullable = false)
+    private GuardianAddress guardianAddress;
+
+    @OneToMany(mappedBy = "guardian", cascade = CascadeType.ALL)
+    private List<Student> students;
+
+    public Guardian() {}
+
+    public Guardian(Long id, String name, String fiscalCode, String relation, List<String> mobileNumber, GuardianAddress guardianAddress) {
+        this.id = id;
+        this.name = name;
+        this.fiscalCode = fiscalCode;
+        this.relation = relation;
+        this.mobileNumber = mobileNumber;
+        this.guardianAddress = guardianAddress;
+    }
+}
