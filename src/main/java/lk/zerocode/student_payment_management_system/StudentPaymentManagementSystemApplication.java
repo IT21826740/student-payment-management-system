2b982b41546a88f03c00f7fb1f8f9fca9aec44b9
IT21826740 <it21826740@my.sliit.lk>
2025-01-02 13:56:22 +0530

    main

---
+++ src/main/java/lk/zerocode/student_payment_management_system/StudentPaymentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.zerocode.student_payment_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPaymentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPaymentManagementSystemApplication.class, args);
+	}
+
+}
