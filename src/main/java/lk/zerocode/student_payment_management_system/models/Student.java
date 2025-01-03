26de6b1ffb570fadd9221b90433a6952862bf4bb
IT21826740 <it21826740@my.sliit.lk>
2025-01-03 14:20:33 +0530

    Create guardian entity

--- src/main/java/lk/zerocode/student_payment_management_system/models/Student.java
+++ src/main/java/lk/zerocode/student_payment_management_system/models/Student.java
@@ -5,7 +5,10 @@ import lk.zerocode.student_payment_management_system.models.options.Gender;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
 @Data
@@ -41,20 +44,36 @@ public class Student {
     @Column(name = "mobile_number")
     private List<String> mobileNumber;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "student_address_id", nullable = false)
-    private StudentAddress studentAddress;
+    @Column(nullable = false)
+    private String email;
 
+    private String address;
+
+//relationship mapping
+
+    //Current Plans
+    // Relationship with PaymentPlans
+    @ManyToOne
+    @JoinColumn(name = "payment_plan_id", nullable = false) // Foreign key column in Student table
+    private PaymentPlans paymentPlan;
+
+    // Relationship with DiscountPlans
+    @ManyToOne
+    @JoinColumn(name = "discount_plan_id", nullable = false) // Foreign key column in Student table
+    private DiscountPlans discountPlan;
+
+    // One-to-Many relationship with Referrals
+    @OneToMany(mappedBy = "referringStudent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Referral> referrals = new ArrayList<>();
+
+    // relationship with guardian
     @ManyToOne
     @JoinColumn(name = "guardian_id", referencedColumnName = "guardian_id", nullable = false)
     private Guardian guardian;
 
-    @Column(nullable = false)
-    private String email;
-
     public Student() {}
 
-    public Student(Long id, String fiscalCode, String name, LocalDate birthday, Gender gender, String country, String specialNeeds, List<String> mobileNumber, StudentAddress studentAddress, String email) {
+    public Student(Long id, String fiscalCode, String name, LocalDate birthday, Gender gender, String country, String specialNeeds, List<String> mobileNumber, String email, String address, PaymentPlans paymentPlan, List<Referral> referrals, Guardian guardian) {
         this.id = id;
         this.fiscalCode = fiscalCode;
         this.name = name;
@@ -63,7 +82,10 @@ public class Student {
         this.country = country;
         this.specialNeeds = specialNeeds;
         this.mobileNumber = mobileNumber;
-        this.studentAddress = studentAddress;
         this.email = email;
+        this.address = address;
+        this.paymentPlan = paymentPlan;
+        this.referrals = referrals;
+        this.guardian = guardian;
     }
 }
