70b5fdfb935a06eec5bebef4662ccc9f5b7720fc
IT21826740 <it21826740@my.sliit.lk>
2025-01-03 16:28:48 +0530

    Create Referral

---
+++ src/main/java/lk/zerocode/student_payment_management_system/models/Referral.java
@@ -0,0 +1,32 @@
+package lk.zerocode.student_payment_management_system.models;
+
+import jakarta.persistence.*;
+import lk.zerocode.student_payment_management_system.models.options.Relation;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "referrals")
+public class Referral {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "referral_id")
+    private Long id;
+
+    //foreign key
+    @ManyToOne
+    @JoinColumn(name = "referring_student_id", nullable = false)
+    private Student referringStudent;
+
+    @ManyToOne
+    @JoinColumn(name = "referred_student_id", nullable = false, unique = true)
+    private Student referredStudent;
+
+    @Column(name = "referral_date", nullable = false)
+    private LocalDate referralDate;
+
+    @Column(name = "relation_or_not")
+    private Relation relation;
+}
